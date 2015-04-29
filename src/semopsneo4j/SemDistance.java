a7533a4d588f8d30cb624468ed17f9fa422f2f61
mael <mael.ogier.38@gmail.com>
2015-04-29 09:37:14 +0200

    Generate closest concepts to every sub-pair of tags from the initial images tags input.

--- src/semopsneo4j/SemDistance.java
+++ src/semopsneo4j/SemDistance.java
@@ -4,13 +4,14 @@ import gexfparserforneo4jdb.neo4j.RelTypes;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
+import java.util.TreeMap;
 
 import org.neo4j.cypher.javacompat.ExecutionEngine;
 import org.neo4j.cypher.javacompat.ExecutionResult;
 import org.neo4j.graphalgo.GraphAlgoFactory;
 import org.neo4j.graphalgo.PathFinder;
 import org.neo4j.graphdb.Direction;
-import org.neo4j.graphdb.DynamicLabel;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Path;
@@ -20,6 +21,8 @@ import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.event.TransactionData;
 import org.neo4j.graphdb.event.TransactionEventHandler;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+
+import treegenerator.services.Inflector;
 
 public class SemDistance {
 	protected String DBname;
@@ -32,6 +35,7 @@ public class SemDistance {
 	
 	public double[][] createMatrix(ArrayList<String> currentTags) {
 		GraphDatabaseService graphDb = connectDB(DBname);
+		Inflector inf = Inflector.getInstance();
 		//Matrix size
 		int nbTags = currentTags.size();
 		double[][] resultMatrix = new double[nbTags][nbTags];
@@ -41,9 +45,9 @@ public class SemDistance {
 		Node node2;
 		double currentDistance=-1.0;
 		for(int i=0;i<nbTags; i++) {
-			node1 = findConceptByURI("base:"+currentTags.get(i), graphDb);
+			node1 = findConceptByURI("base:"+inf.singularize(currentTags.get(i)), graphDb);
 			for(int j=i; j<nbTags; j++){
-				node2 = findConceptByURI("base:"+currentTags.get(j), graphDb);
+				node2 = findConceptByURI("base:"+inf.singularize(currentTags.get(j)), graphDb);
 				if(node1!=null && node2!=null)
 					currentDistance = wuPalmerEvolvedMeasure(node1, node2, graphDb);
 				else
@@ -97,30 +101,32 @@ public class SemDistance {
 		return nodesResult;
 	}
 
-	private double wuPalmerMeasure(Node node1, Node node2, Node rootNode, GraphDatabaseService graphDb){
-		double result = -1.0;
-		Transaction tx = graphDb.beginTx();
-		try {	
-			Node lca = findLCA(node1, node2, graphDb);
-			System.out.println("LCA for " + node1.getProperty("uri") + " and "+node2.getProperty("uri") + " is : " + lca.getProperty("uri"));
-			PathFinder<Path> finder = GraphAlgoFactory.shortestPath(
-					PathExpanders.forTypesAndDirections( RelTypes.PARENT, Direction.INCOMING, RelTypes.EQUIV, Direction.BOTH ), 20);
-			int lcaToRoot = finder.findSinglePath(rootNode,lca).length();
-			//Substract 1 cause base:xxx count as 0
-			int node1toLCA = finder.findSinglePath(lca, node1).length()-1;
-			int node2toLCA = finder.findSinglePath(lca, node2).length()-1;
-			result = (double) (2*lcaToRoot)/(2*lcaToRoot+node1toLCA+node2toLCA);
-			tx.success();
-		} finally {
-			tx.close();			
-		}
-		return result;
-	}
+//	private double wuPalmerMeasure(Node node1, Node node2, Node rootNode, GraphDatabaseService graphDb){
+//		double result = -1.0;
+//		Transaction tx = graphDb.beginTx();
+//		try {	
+//			Node lca = findLCA(node1, node2, graphDb);
+//			System.out.println("LCA for " + node1.getProperty("uri") + " and "+node2.getProperty("uri") + " is : " + lca.getProperty("uri"));
+//			PathFinder<Path> finder = GraphAlgoFactory.shortestPath(
+//					PathExpanders.forTypesAndDirections( RelTypes.PARENT, Direction.INCOMING, RelTypes.EQUIV, Direction.BOTH ), 20);
+//			int lcaToRoot = finder.findSinglePath(rootNode,lca).length();
+//			//Substract 1 cause base:xxx count as 0
+//			int node1toLCA = finder.findSinglePath(lca, node1).length()-1;
+//			int node2toLCA = finder.findSinglePath(lca, node2).length()-1;
+//			result = (double) (2*lcaToRoot)/(2*lcaToRoot+node1toLCA+node2toLCA);
+//			tx.success();
+//		} finally {
+//			tx.close();			
+//		}
+//		return result;
+//	}
 	
 	private double wuPalmerEvolvedMeasure(Node node1, Node node2, GraphDatabaseService graphDb) {
 		double result = -1.0;
 		Transaction tx = graphDb.beginTx();
 		try {
+			if(node1.equals(node2))
+				return 1.0;
 			
 			Node lca = findLCA(node1, node2, graphDb);
 			if(lca==null){
@@ -130,19 +136,14 @@ public class SemDistance {
 			System.out.println("LCA for " + node1.getProperty("uri") + " and "+node2.getProperty("uri") + " is : ");
 			System.out.println(lca.getProperty("uri"));
 			//Wu-Palmer data
-			PathFinder<Path> finder = GraphAlgoFactory.shortestPath(
-					PathExpanders.forTypesAndDirections( 
-							RelTypes.PARENT, Direction.INCOMING, 
-							RelTypes.EQUIV, Direction.BOTH, 
-							RelTypes.VIRTUAL, Direction.INCOMING ), 20);
-			Path lcaToRootPath = finder.findSinglePath(rootNode,lca);
+			Path lcaToRootPath = findShortestPath(rootNode,lca);
 			if(lcaToRootPath==null)
 				return result;
 			
 			int lcaToRoot = lcaToRootPath.length()-1;
 			//Substract 1 cause base:xxx count as 0
-			int node1toLCA = finder.findSinglePath(lca, node1).length()-1;
-			int node2toLCA = finder.findSinglePath(lca, node2).length()-1;
+			int node1toLCA = findShortestPath(lca, node1).length()-1;
+			int node2toLCA = findShortestPath(lca, node2).length()-1;
 			
 			//Evolved
 			PathFinder<Path> finderLong = GraphAlgoFactory.allPaths(
@@ -167,6 +168,15 @@ public class SemDistance {
 		
 		
 		return result;
+	}
+	
+	private Path findShortestPath(Node node1, Node node2) {
+		PathFinder<Path> finder = GraphAlgoFactory.shortestPath(
+				PathExpanders.forTypesAndDirections( 
+						RelTypes.PARENT, Direction.INCOMING, 
+						RelTypes.EQUIV, Direction.BOTH, 
+						RelTypes.VIRTUAL, Direction.INCOMING ), 20);
+		return finder.findSinglePath(node1,node2);
 	}
 	
 	private GraphDatabaseService connectDB(String DBname) {
@@ -242,18 +252,63 @@ public class SemDistance {
 	        }
 	    } );
 	}
-
+	
+	//Check which tags already exist in the Neo4j DB
 	public ArrayList<String> checkTags(ArrayList<String> currentTags) {
 		GraphDatabaseService graphDb = connectDB(DBname);
+		Inflector inf = Inflector.getInstance();
 		ArrayList<String> tagsToCreate = new ArrayList<>();
 		String tag;
 		for(int i=0; i<currentTags.size(); i++) {
 			tag = currentTags.get(i);
-			if (findConceptByURI("base:"+tag, graphDb)==null){
-				tagsToCreate.add(tag);
+			if (findConceptByURI("base:"+inf.singularize(tag), graphDb)==null){
+				tagsToCreate.add(inf.singularize(tag));
 			}
 		}
 		graphDb.shutdown();
 		return tagsToCreate;
+	}
+	
+	//Select the 10 best related concepts to a pair of tags
+	public ArrayList<String> findTenConcepts(ArrayList<String> tagSubList) {
+		GraphDatabaseService graphDb = connectDB(DBname);
+		TreeMap<Integer,String> orderedConcepts = new TreeMap<Integer,String>();
+		Inflector inf = Inflector.getInstance();
+		
+		Transaction tx = graphDb.beginTx();
+		try {
+			Node node1 = findConceptByURI("base:"+inf.singularize(tagSubList.get(0)), graphDb);
+			Node node2 = findConceptByURI("base:"+inf.singularize(tagSubList.get(1)), graphDb);
+			Node lca = findLCA(node1, node2, graphDb);
+			
+			//Add maximum ten concepts, lca will obviously be the most interesting 
+			//Score = distance(node1, lca) + distance (node2, lca)
+			
+			//Handle LCA's case
+			if(lca!=null){
+				Integer scoreLCA = findShortestPath(lca, node1).length() + findShortestPath(lca, node2).length() - 2;
+				
+			    //Take shortest path between lca and root and add higher concepts
+			    Path lcaToRoot = findShortestPath(rootNode,lca);
+			    Integer currentScore = scoreLCA + lcaToRoot.length()*2;
+			    if(lcaToRoot!=null){
+			    	Iterator<Node> it = lcaToRoot.nodes().iterator();
+			    	it.next(); //skip virtual:top
+				    while(it.hasNext()){
+				    	Node currentNode = it.next();
+				    	currentScore = currentScore - 2;
+				    	orderedConcepts.put(currentScore, (String)currentNode.getProperty("uri"));
+				    }
+			    }
+			}
+			System.out.println("Tags sublist : " + tagSubList);
+			System.out.println(orderedConcepts);
+			tx.success();
+		} finally {
+			tx.close();
+		}
+		
+		graphDb.shutdown();
+		return null;
 	}
 }
--- src/semopsneo4j/SemOpsNeo4j.java
+++ src/semopsneo4j/SemOpsNeo4j.java
@@ -22,6 +22,7 @@ import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import org.neo4j.graphdb.schema.Schema;
+import org.sqlite.SQLiteConfig.TempStore;
 
 import treegenerator.TreeGenerator;
 import treegenerator.model.OnlineConcept;
@@ -34,7 +35,15 @@ public class SemOpsNeo4j {
 	private static String GEXF_DIR_PATH;
 	private static String MATRIXES_DIR_PATH;
 	
+	//Do you need a .gexf file with all new concepts/relationships ?
 	static final boolean GENERATE_GEXF_FILE = false;
+	
+	//Should we generate a global similarity matrix with all base concepts ?
+	static final boolean GENERATE_FULL_MATRIX = false;
+	
+	//Should we generate each image's similarity matrix ?
+	static final boolean GENERATE_IMAGES_MATRIX = true;
+	
 	/**
 	 * @param args
 	 * @throws Exception 
@@ -62,7 +71,7 @@ public class SemOpsNeo4j {
 		ArrayList<String> tagsToCreate;
 		double[][] distanceMatrix;
 		Iterator itMap = imagesTags.entrySet().iterator();
-
+		String stringTags="";
 		while (itMap.hasNext()) {
 			treeGen = new TreeGenerator();
 			gexfParser = new GexfParserForNeo4jDB();
@@ -74,7 +83,7 @@ public class SemOpsNeo4j {
 			tagsToCreate = semDistance.checkTags(currentTags);
 
 			//Eventually create tags
-			String stringTags = StringUtils.join(tagsToCreate,SEPARATOR);
+			stringTags = StringUtils.join(tagsToCreate,SEPARATOR);
 			if(stringTags!=""){
 				
 				if(GENERATE_GEXF_FILE){
@@ -89,14 +98,50 @@ public class SemOpsNeo4j {
 			}
 			
 			//Compute and print distance matrix for each image
-			distanceMatrix = semDistance.createMatrix(currentTags);
-			printCSV(distanceMatrix,pairFromMap,MATRIXES_DIR_PATH);			
+			if(GENERATE_IMAGES_MATRIX){
+				distanceMatrix = semDistance.createMatrix(currentTags);
+				printCSV(distanceMatrix,pairFromMap,MATRIXES_DIR_PATH);	
+			}	
 		}		
 		
 		//Compute and print distance matrix for all base's tags
-		ArrayList<String> allBaseConcepts = semDistance.findAllBaseTags();
-		double[][] allTagsDistanceMatrix = semDistance.createMatrixAllBase(allBaseConcepts);
-		printCSV(allTagsDistanceMatrix,allBaseConcepts,MATRIXES_DIR_PATH);	
+		if(GENERATE_FULL_MATRIX){
+			ArrayList<String> allBaseConcepts = semDistance.findAllBaseTags();
+			double[][] allTagsDistanceMatrix = semDistance.createMatrixAllBase(allBaseConcepts);
+			printCSV(allTagsDistanceMatrix,allBaseConcepts,MATRIXES_DIR_PATH);	
+		}
+		
+		//Generate ranked lists of interesting tags
+		Iterator itMapSubLists = imagesTags.entrySet().iterator();
+		ArrayList<ArrayList<String>> currentImageSubTagLists;
+		ArrayList<String> currentImageTags;
+		
+		while(itMapSubLists.hasNext()) {
+			Map.Entry pairFromMap = (Map.Entry) itMapSubLists.next();
+			
+			//Split tags into sublists of 2 elements
+			currentImageTags = (ArrayList<String>) pairFromMap.getValue();
+			currentImageSubTagLists = splitTags(currentImageTags);
+			
+			//Find the 10 most interesting concepts for each pair of tags
+			for(int i=0;i<currentImageSubTagLists.size();i++) {
+				ArrayList<String> tenTagsForThisSubList = semDistance.findTenConcepts(currentImageSubTagLists.get(i));
+			}
+		}
+	}
+
+	private static ArrayList<ArrayList<String>> splitTags(ArrayList<String> currentImageTags) {
+		ArrayList<ArrayList<String>> returnList = new ArrayList<ArrayList<String>>();
+		ArrayList<String> tempList;
+		for(int i=0; i<currentImageTags.size()-1;i++){
+			for(int j=i+1; j<currentImageTags.size();j++){
+				tempList = new ArrayList<String>(2);
+				tempList.add(currentImageTags.get(i));
+				tempList.add(currentImageTags.get(j));
+				returnList.add(tempList);
+			}
+		}		
+		return returnList;
 	}
 
 	private static void loadProperties(String fileConfig) throws FileNotFoundException, IOException {
@@ -151,17 +196,17 @@ public class SemOpsNeo4j {
 
         int size = distanceMatrix.length;
         //Header
-        file.print(", ");
+        file.print(",");
         for (int i = 0; i < size; i++) {
-			file.print(allBaseConcepts.get(i).substring(5)+ ", ");
+			file.print(allBaseConcepts.get(i).substring(5)+ ",");
 		}
         file.println("");
         
         //Content
 		for (int j = 0; j < size; j++) {
-			file.print(allBaseConcepts.get(j).substring(5) + ", ");
+			file.print(allBaseConcepts.get(j).substring(5) + ",");
 			for (int i = 0; i < size; i++) {
-				file.print(distanceMatrix[i][j] + ", ");
+				file.print(distanceMatrix[i][j] + ",");
 			}
 
 			file.println("");
@@ -175,18 +220,18 @@ public class SemOpsNeo4j {
 
         int size = distanceMatrix.length;
         //Header
-        file.print(", ");
+        file.print(",");
         for (int i = 0; i < size; i++) {
-			file.print(((ArrayList<String>) pairFromMap.getValue()).get(i) + ", ");
+			file.print(((ArrayList<String>) pairFromMap.getValue()).get(i) + ",");
 		}
         file.println("");
         
         //Content
 		for (int j = 0; j < size; j++) {
-			file.print(((ArrayList<String>) pairFromMap.getValue()).get(j) + ", ");
+			file.print(((ArrayList<String>) pairFromMap.getValue()).get(j) + ",");
 
 			for (int i = 0; i < size; i++) {
-				file.print(distanceMatrix[i][j] + ", ");
+				file.print(distanceMatrix[i][j] + ",");
 			}
 
 			file.println("");
